38979328577f7dae86bbd8067f82af5476467233
mathemartins <martins>
2018-01-20 18:05:41 +0100

    ok

--- dev/settings/local.py
+++ dev/settings/local.py
@@ -183,9 +183,9 @@ DATABASES = {
     }
 }
 
-# import dj_database_url
-# db_from_env = dj_database_url.config()
-# DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # CORS_REPLACE_HTTPS_REFERER       = True
 # HOST_SCHEME                      = "https://"
