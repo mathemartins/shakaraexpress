1a0d665762597afae044c3a06f5cecfcdf38590c
mathemartins <martins>
2018-03-14 10:26:11 +0100

    ok

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -4,6 +4,9 @@ from newsletter.models import Newsletter
 
 
 class NewsletterModelForm(forms.ModelForm):
+
+	email = forms.CharField(label='', widget=forms.TextInput(attrs={"placeholder": "example@example.com", 'class':'form-control'}))
+
 	class Meta:
 		model = Newsletter
 		fields = ('email',)
