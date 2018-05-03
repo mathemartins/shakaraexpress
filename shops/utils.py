b478b4b99acbbae947d9665e5272b2dec61ffe6d
mathemartins <martins>
2018-05-03 09:38:29 +0100

    pay with quickteller integration

--- shops/utils.py
+++ shops/utils.py
@@ -9,3 +9,7 @@ from django.utils.html import strip_tags
 
 def shop_code_generator(size=8, chars=string.ascii_lowercase + string.digits):
 	return ''.join(random.choice(chars) for _ in range(size))
+
+
+def transaction_ref(size=10, chars=string.ascii_lowercase + string.digits):
+	return ''.join(random.choice(chars) for _ in range(size))
